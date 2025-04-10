51d1644b0d7697c5201b7b67bf30d39bcfe5cf6f
wgg76 <167985626+wgg76@users.noreply.github.com>
2025-04-10 10:44:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"; // ✅ Importante
+import { BrowserRouter } from "react-router-dom"; 
 import { ItemProvider } from "./context/ItemContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
