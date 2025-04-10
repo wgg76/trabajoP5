56386853b2164a64e9d7b6452880d66359f4a4ba
wgg76 <167985626+wgg76@users.noreply.github.com>
2025-04-10 10:36:25 -0300

    Update api.js

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,4 +1,3 @@
-// src/utils/api.js
 export const fetchCharacters = async () => {
   try {
     const response = await fetch("https://cdn.jsdelivr.net/gh/wgg76/superhero-api@main/superheroes.json");
@@ -6,7 +5,7 @@ export const fetchCharacters = async () => {
       throw new Error("Error al obtener personajes");
     }
     const data = await response.json();
-    // Retornamos directamente el array de superhéroes
+   
     return data;
   } catch (error) {
     throw error;
@@ -20,7 +19,7 @@ export const fetchCharactersByName = async (name) => {
       throw new Error("Error al obtener personajes");
     }
     const data = await response.json();
-    // Filtramos los personajes cuyo nombre incluya el término buscado (ignora mayúsculas/minúsculas)
+  
     return data.filter((character) =>
       character.name.toLowerCase().includes(name.toLowerCase())
     );
