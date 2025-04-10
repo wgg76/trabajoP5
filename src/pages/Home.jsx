f2b7bfdeba253dbdc41c75df0064b595856c8ba2
wgg76 <167985626+wgg76@users.noreply.github.com>
2025-04-10 10:35:21 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -97,13 +97,6 @@ const Home = () => {
 
         {/* Contenedor de botones en fila */}
         <div className="flex flex-row gap-4 justify-center">
-          <button
-            onClick={handleBulkUpload}
-            className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded"
-            disabled={loading} // Deshabilitar el botón mientras se está cargando
-          >
-            {loading ? "Cargando..." : "Cargar personajes"}
-          </button>
           <Link
             to="/items"
             className="bg-green-600 hover:bg-green-700 text-white px-6 py-2 rounded"
